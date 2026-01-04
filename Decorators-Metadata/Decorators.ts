8873271b7c0c7abd38b0e11f83d0c8e12fb1a7d6
leeuwindave732-lang <leeuwindave732@gmail.com>
2026-01-04 17:38:10 +0800

    tsconfig.json

---
+++ Decorators-Metadata/Decorators.ts
@@ -0,0 +1,22 @@
+/*
+    What are decorators?
+    Decorators are special functions that can be attached to classes,
+    methods, properties, or parameters to modify behavior or add metadata. 
+    They are mostly used for:
+
+    Logging
+    Validation
+    Access control
+    Dependency injection
+
+    Enable decorators in TypeScript
+    In your tsconfig.json:
+
+    {
+        "experimentalDecorators": true,
+        "emitDecoratorMetadata": true
+    }
+
+    if you dont have one just add it in compiler options in tsconfig.json
+
+*/
